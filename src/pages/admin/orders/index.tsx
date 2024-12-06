f19ace36d72ae0a5f17a98c0b37c9e5861fd5425
zoir-dev <zoirjonabduvohidov0@gmail.com>
2024-12-06 15:27:01 +0500

    some changes

--- src/pages/admin/orders/index.tsx
+++ src/pages/admin/orders/index.tsx
@@ -1,46 +1,54 @@
-import { useInfiniteGet } from "@/hooks/useInfiniteGet"
-import Loading from "@/layouts/loading"
-import AdminOrderCard from "./admin-order-card"
+import { useInfiniteGet } from "@/hooks/useInfiniteGet";
+import Loading from "@/layouts/loading";
+import AdminOrderCard from "./admin-order-card";
 import { useSearch } from "@tanstack/react-router";
 import ParamAnimatedTabs from "@/components/param/animated-tab";
 
 export default function Orders() {
     const search: any = useSearch({ from: "__root__" });
-    const {data,isLoading}=useInfiniteGet<{id:number,quantity:number,status:number,updated_at:string,cart:{id:number,order:1,product:Product,quantity:number}[]}>("order/?activated=true&delivering=true&pending=true",{...search,status:undefined})
-  return (
-    <div className="pt-4">
-        <h2 className="text-lg sm:text-xl md:text-2xl font-medium border-b pb-2 mb-4">Buyurtmalar</h2>
-        <ParamAnimatedTabs options={[
-            {
-              name: "Barcha buyurtmalar",
-              id: 0,
-            },
-            {
-              name: "To'lov qilinmagan",
-              id: 10,
-            },
-            {
-              name: "Faol",
-              id: 1,
-            },
-            {
-              name:"Yetkazilmoqda",
-              id:"2"
-            },
-        ]} paramName="status"/>
-        <Loading loading={isLoading}>
-        <div className="flex flex-col gap-4">
-          {data
-            ?.filter((d) =>
-              search.status
-                ? d.status == (search.status == 10 ? 0 : search.status)
-                : true
-            )
-            ?.map((d, i: number) => (
-                <AdminOrderCard key={i} p={d} /> 
-            ))}
+    const { data, isLoading } = useInfiniteGet<{
+        id: number;
+        quantity: number;
+        status: number;
+        updated_at: string;
+        cart: { id: number; order: 1; product: Product; quantity: number }[];
+    }>("order/?activated=true&delivering=true&pending=true", {
+        ...search,
+        status: undefined,
+    });
+    return (
+        <div className="pt-4">
+            <h2 className="text-lg sm:text-xl md:text-2xl font-medium border-b pb-2 mb-4">
+                Buyurtmalar
+            </h2>
+            <ParamAnimatedTabs
+                options={[
+                    {
+                        name: "Barcha buyurtmalar",
+                        id: 0,
+                    },
+                    {
+                        name: "To'lov qilinmagan",
+                        id: 10,
+                    },
+                    {
+                        name: "Faol",
+                        id: 1,
+                    },
+                    {
+                        name: "Yetkazilmoqda",
+                        id: "2",
+                    },
+                ]}
+                paramName="status"
+            />
+            <Loading loading={isLoading}>
+                <div className="flex flex-col gap-4">
+                    {data?.map((d, i: number) => (
+                        <AdminOrderCard key={i} p={d} />
+                    ))}
+                </div>
+            </Loading>
         </div>
-        </Loading>
-    </div>
-  )
+    );
 }
--- src/pages/profile/order/order-card.tsx
+++ src/pages/profile/order/order-card.tsx
@@ -1,15 +1,15 @@
 import Image from "@/components/custom/image";
 import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
 } from "@/components/ui/accordion";
 import { Button } from "@/components/ui/button";
 import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
+    Carousel,
+    CarouselContent,
+    CarouselItem,
 } from "@/components/ui/carousel";
 import { useConfirm } from "@/hooks/useConfirm";
 import { useRequest } from "@/hooks/useRequest";
@@ -23,136 +23,149 @@ import { useRef } from "react";
 import { toast } from "sonner";
 
 export default function OrderCard({
-  p,
+    p,
 }: {
-  p: {
-    id: number;
-    quantity: number;
-    status: number;
-    updated_at: string;
-    cart: { id: number; order: 1; product: Product; quantity: number }[];
-  };
+    p: {
+        id: number;
+        quantity: number;
+        status: number;
+        updated_at: string;
+        cart: { id: number; order: 1; product: Product; quantity: number }[];
+    };
 }) {
-  const plugin = useRef(Autoplay({ delay: 3000 }));
-  const fade = useRef(Fade());
+    const plugin = useRef(Autoplay({ delay: 3000 }));
+    const fade = useRef(Fade());
 
-  const confirm=useConfirm()
-  const search: any = useSearch({ from: "__root__" });
-  const {patch}=useRequest()
-  const queryClient=useQueryClient()
+    const confirm = useConfirm();
+    const search: any = useSearch({ from: "__root__" });
+    const { patch } = useRequest();
+    const queryClient = useQueryClient();
 
-  async function handleCancel(id: number) {
-    const isConfirmed = await confirm({
-      title: "Buyurtmani bekor qilinsinmi?",
-    });
-    if (isConfirmed){
-      toast.promise(
-        patch(`order/${id}/`,{status:3}),
-        {
-          loading: "O'zgartirilmoqda...",
-          success: () => {
-            queryClient.invalidateQueries({queryKey:["order/",search]})
-            return "Muvaffaqiyatli bekor qilindi";
-          },
+    async function handleCancel(id: number) {
+        const isConfirmed = await confirm({
+            title: "Buyurtmani bekor qilinsinmi?",
+        });
+        if (isConfirmed) {
+            toast.promise(patch(`order/${id}/`, { status: 3 }), {
+                loading: "O'zgartirilmoqda...",
+                success: () => {
+                    queryClient.invalidateQueries({
+                        queryKey: ["order/", search],
+                    });
+                    return "Muvaffaqiyatli bekor qilindi";
+                },
+            });
         }
-      );
     }
-  }
 
-  return (
-    <Accordion
-      type="single"
-      collapsible
-      className="w-full bg-background p-4 rounded"
-    >
-      <div className="flex items-center justify-between pb-4">
-        <p>
-          Buyurtma holati:{" "}
-          <span className="text-foreground font-semibold">
-            {statuses[p.status as 0 | 1 | 2 | 3 | 4]}
-          </span>
-        </p>
-        <p>
-          Yetkazilish (Yetkazilgan) sanasi:{" "}
-          <span className="text-foreground font-semibold">
-            {p.updated_at.split("T")[0]}
-          </span>
-        </p>
-      </div>
-      {p.cart?.map((c,i) => (
-        <AccordionItem value={c.id?.toString()} key={i}>
-          <AccordionTrigger>
-            <div className="flex flex-col items-start gap-2">
-              <p className="font-normal text-muted-foreground">
-                Buyurtma id raqami{" "}
-                <span className="text-foreground font-semibold">{c.id}</span>
-              </p>
-              <p className="font-normal text-muted-foreground">
-                Soni:{" "}
-                <span className="text-foreground font-semibold">
-                  {c.quantity}
-                </span>
-              </p>
+    return (
+        <Accordion
+            type="single"
+            collapsible
+            className="w-full bg-background p-4 rounded"
+        >
+            <div className="flex items-center justify-between pb-4">
+                <p>
+                    Buyurtma holati:{" "}
+                    <span className="text-foreground font-semibold">
+                        {statuses[p.status as 0 | 1 | 2 | 3 | 4]}
+                    </span>
+                </p>
+                <p>
+                    Yetkazilish (Yetkazilgan) sanasi:{" "}
+                    <span className="text-foreground font-semibold">
+                        {p.updated_at.split("T")[0]}
+                    </span>
+                </p>
             </div>
-          </AccordionTrigger>
-          <AccordionContent>
-            <div className="flex items-center justify-between gap-6">
-            <div className="flex items-center gap-4">
-              <Carousel
-                className="w-32"
-                plugins={[plugin.current, fade.current]}
-              >
-                <CarouselContent>
-                  {[
-                    c.product.image1,
-                    c.product.image2,
-                    c.product.image3,
-                    c.product.image4,
-                  ]?.map((i) => (
-                    <CarouselItem key={i}>
-                      <Image
-                        src={i}
-                        alt={c.product.name}
-                        width={100}
-                        className="w-full h-full object-cover"
-                      />
-                    </CarouselItem>
-                  ))}
-                </CarouselContent>
-              </Carousel>
-              <div className="flex flex-col gap-2 items-start">
-                <div className="flex items-center gap-2 text-base">
-                  <p className="font-semibold text-muted-foreground">Nomi: </p>
-                  <p>{c.product?.name}</p>
-                </div>
-                <div className="flex items-center gap-2 text-base">
-                  <p className="font-semibold text-muted-foreground">Narxi: </p>
-                  <p>
-                    {formatMoney(
-                      c.product?.discounted_price || c.product?.price,
-                      undefined,
-                      true
-                    )}
-                  </p>
-                </div>
-                <div className="flex items-center gap-2">
-                  <p>{c.product?.description}</p>
-                </div>
-              </div>
-          </div>
-          <Button icon={<Trash2 width={18}/>} variant='ghost' className="!text-destructive" onClick={()=>handleCancel(p.id)}/>
-          </div>
-        </AccordionContent>
-        </AccordionItem>
-      ))}
-    </Accordion>
-  );
+            {p.cart?.map((c, i) => (
+                <AccordionItem value={c.id?.toString()} key={i}>
+                    <AccordionTrigger>
+                        <div className="flex flex-col items-start gap-2">
+                            <p className="font-normal text-muted-foreground">
+                                Buyurtma id raqami{" "}
+                                <span className="text-foreground font-semibold">
+                                    {c.id}
+                                </span>
+                            </p>
+                            <p className="font-normal text-muted-foreground">
+                                Soni:{" "}
+                                <span className="text-foreground font-semibold">
+                                    {c.quantity}
+                                </span>
+                            </p>
+                        </div>
+                    </AccordionTrigger>
+                    <AccordionContent>
+                        <div className="flex items-center justify-between gap-6">
+                            <div className="flex items-center gap-4">
+                                <Carousel
+                                    className="w-32"
+                                    plugins={[plugin.current, fade.current]}
+                                >
+                                    <CarouselContent>
+                                        {[
+                                            c.product.image1,
+                                            c.product.image2,
+                                            c.product.image3,
+                                            c.product.image4,
+                                        ]?.map((i) => (
+                                            <CarouselItem key={i}>
+                                                <Image
+                                                    src={i}
+                                                    alt={c.product.name}
+                                                    width={100}
+                                                    className="w-full h-full object-cover"
+                                                />
+                                            </CarouselItem>
+                                        ))}
+                                    </CarouselContent>
+                                </Carousel>
+                                <div className="flex flex-col gap-2 items-start">
+                                    <div className="flex items-center gap-2 text-base">
+                                        <p className="font-semibold text-muted-foreground">
+                                            Nomi:{" "}
+                                        </p>
+                                        <p>{c.product?.name}</p>
+                                    </div>
+                                    <div className="flex items-center gap-2 text-base">
+                                        <p className="font-semibold text-muted-foreground">
+                                            Narxi:{" "}
+                                        </p>
+                                        <p>
+                                            {formatMoney(
+                                                c.product?.discounted_price ||
+                                                    c.product?.price,
+                                                undefined,
+                                                true
+                                            )}
+                                        </p>
+                                    </div>
+                                    <div className="flex items-center gap-2">
+                                        <p>{c.product?.description}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            {![2, 3, 4].includes(p.status) && (
+                                <Button
+                                    icon={<Trash2 width={18} />}
+                                    variant="ghost"
+                                    className="!text-destructive"
+                                    onClick={() => handleCancel(p.id)}
+                                />
+                            )}
+                        </div>
+                    </AccordionContent>
+                </AccordionItem>
+            ))}
+        </Accordion>
+    );
 }
 
 const statuses = {
-  0: "To'lov qilinmagan",
-  1: "Faol",
-  2: "Yetkazilmoqda",
-  3: "Bekor qilingan",
-  4: "Yetkazilgan",
+    0: "To'lov qilinmagan",
+    1: "Faol",
+    2: "Yetkazilmoqda",
+    3: "Bekor qilingan",
+    4: "Yetkazilgan",
 };
